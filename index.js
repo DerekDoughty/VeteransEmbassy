603579d5970fc9195461e1bc9672e2ca299da35f
DerekDoughty <Doughty326@gmail.com>
2015-08-18 11:40:01 -0700

    post method works on sign up page.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 $(function (){
 
+var $post = $.post
+
+	$post('http://localhost:3000/users', { 
+		userName: "Ryan",
+		email: "ryan@ryan.com",
+		password: "ryanhasNiceHair",		
+	})
+		.done(function(){
+			console.log('done')
+		})
+		.fail(function(){
+			alert('fucked up')
+		})
 })
