1024d6c27b5715f9b93ac0230b5dc292306f8cbd
DerekDoughty <Doughty326@gmail.com>
2015-09-09 14:22:02 -0700

    new branch for gh pages

--- hikingEvents.js
+++ hikingEvents.js
@@ -39,5 +39,20 @@ $('.hiking-events').on('submit','.delete-events', function(event){
 	})     
 })
 
+var googleapis = 
+console.log($('#map'))
+map = new googleapis({
+  div: '#map',
+  // zoom: 16,
+  // lat: -12.043333,
+  // lng: -77.028333,
+  click: function(e) {
+    alert('click');
+  },
+  dragend: function(e) {
+    alert('dragend');
+  }
+})
+
 })
  
